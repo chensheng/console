cdd79b09bd8f43b464c4f850cd6b7eaf577d5fd2
chensheng <742611497@qq.com>
2021-10-25 09:57:45 +0800

    fix bugs

--- src/stores/nacos.js
+++ src/stores/nacos.js
@@ -3,53 +3,59 @@ import { observable, action } from 'mobx'
 import { Notify } from '@kube-design/components'
 
 export default class NacosStore {
-    @observable
-    isLoading = false  
+  @observable
+  isLoading = false
 
-    @observable
-    isEditing = false
-    
-    @observable
-    configContent = ''
-    
-    @action
-    async fetchConfig(nacosInfo = { url, username, password }, params = { tenant, dataId, group }) {
-        this.isLoading = true
-        const url = "nacos/v1/cs/configs"
-        const headers = {
-            'nacos-url': nacosInfo.url,
-            'nacos-username': nacosInfo.username,
-            'nacos-password': nacosInfo.password
-        }
-        
-        const result = await request.get(url, params, {headers}, (error) => {
-            Notify.error(t('Error Tips'), t(error.message))
-        })
-        this.configContent = result ? result : ''
-        this.isLoading = false
-        return result
+  @observable
+  isEditing = false
+
+  @observable
+  configContent = ''
+
+  @action
+  async fetchConfig(
+    nacosInfo = { url, username, password },
+    params = { tenant, dataId, group }
+  ) {
+    this.isLoading = true
+    const url = 'nacos/v1/cs/configs'
+    const headers = {
+      'nacos-url': nacosInfo.url,
+      'nacos-username': nacosInfo.username,
+      'nacos-password': nacosInfo.password,
     }
 
-    @action
-    async saveConfig(nacosInfo = { url, username, password }, params = { tenant, dataId, group, content, type }) {
-        this.isLoading = true
-        const url = "nacos/v1/cs/configs"
-        const headers = {
-            'content-type': 'application/x-www-form-urlencoded',
-            'nacos-url': nacosInfo.url,
-            'nacos-username': nacosInfo.username,
-            'nacos-password': nacosInfo.password
-        }
+    const result = await request.get(url, params, { headers }, error => {
+      Notify.error(t('Error Tips'), t(error.message))
+    })
+    this.configContent = result || ''
+    this.isLoading = false
+    return result
+  }
 
-        await request.post(url, params, {headers}, (error) => {
-            Notify.error(t('Error Tips'), t(error.message))
-        })
-        this.isLoading = false
+  @action
+  async saveConfig(
+    nacosInfo = { url, username, password },
+    params = { tenant, dataId, group, content, type }
+  ) {
+    this.isLoading = true
+    const url = 'nacos/v1/cs/configs'
+    const headers = {
+      'content-type': 'application/x-www-form-urlencoded',
+      'nacos-url': nacosInfo.url,
+      'nacos-username': nacosInfo.username,
+      'nacos-password': nacosInfo.password,
     }
 
-    @action
-    reset() {
-        this.isEditing = false
-        this.configContent = ''
-    }
+    await request.post(url, params, { headers }, error => {
+      Notify.error(t('Error Tips'), t(error.message))
+    })
+    this.isLoading = false
+  }
+
+  @action
+  reset() {
+    this.isEditing = false
+    this.configContent = ''
+  }
 }
